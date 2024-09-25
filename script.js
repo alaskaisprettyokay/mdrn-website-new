270ca523f295875140b6f3f69ff2c3d3ec4dd37e
Miranda <miranda93_3@hotmail.com>
2024-09-25 23:42:13 +0200

    Sign in mail

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 sum += dataArray[i];
             }
             const average = sum / (range.end - range.start + 1);
-            const size = (average / 255) * 40 + initialSizes[index]; // Ajustar el tamaño del círculo
+            const size = (average / 255) * 30 + initialSizes[index]; // Ajustar el tamaño del círculo
             circle.style.width = `${size}vmin`;
             circle.style.height = `${size}vmin`;
         });
@@ -82,5 +82,32 @@ document.addEventListener('DOMContentLoaded', function() {
     circles.forEach((circle, index) => {
         circle.style.width = `${initialSizes[index]}vmin`;
         circle.style.height = `${initialSizes[index]}vmin`;
+    });
+
+    const form = document.getElementById('email-form');
+    const input = document.getElementById('email-input');
+    const button = document.getElementById('send-button');
+    const confirmationText = document.getElementById('confirmation-text');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        // Animar la desaparición del input y el botón
+        input.classList.add('shrink');
+        button.classList.add('shrink');
+
+        // Esperar a que termine la animación de desaparición
+        setTimeout(() => {
+            // Ocultar completamente el input y el botón
+            input.style.display = 'none';
+            button.style.display = 'none';
+
+            // Mostrar y animar el texto de confirmación
+            confirmationText.classList.remove('hidden');
+          //  setTimeout(() => {
+                confirmationText.style.opacity = '1';
+                confirmationText.classList.add('typing');
+          //  }, 50);
+        }, 500);
     });
 });
